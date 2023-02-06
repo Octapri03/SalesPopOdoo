044355003e4341b9e4838ec55007d821bb7e12ea
jasaes <jaumetsmr@gmail.com>
2023-02-06 12:01:50 +0000

    hi ha errors: invf = comodel._fields[self.inverse_name] KeyError: 'product'

--- models/models.py
+++ models/models.py
@@ -17,23 +17,33 @@ class user(models.Model):
     onSale = fields.One2many('salespop.product', 'seller')
     favs = fields.One2many('salespop.product', 'fav')
 
+    isUserPop = fields.Boolean(default = False)
+
 class product(models.Model):
     _name = 'salespop.product'
     _description = 'salespop products'
 
-    name = fields.Char()
-    price = fields.Integer()
-    description = fields.Char()
-    ubication = fields.Char()
-    publicationDate = fields.Datetime(readonly=True, default=fields.Datetime.now)
     seller = fields.Many2one('res.partner', ondelete = "cascade")
+    onSale = fields.One2many('salespop.product', 'seller')#odoo.fields.Selection
+    name = fields.Char()
+    description = fields.Char()
+    publicationDate = fields.Datetime(readonly=True, default=fields.Datetime.now)
+    label = fields.Many2one('salespop.label')
+    image = fields.One2many('salespop.photo', 'product')
+    price = fields.Integer()
+    ubication = fields.Char()
+  
     fav = fields.Many2one('res.partner')
+    message = fields.One2many('salespop.message', 'product')
+   
 
 class category(models.Model):
     _name = 'salespop.category'
     _description = 'salespop categories'
 
     name = fields.Char()
+    product = fields.One2many('salespop.product', 'category')
+    image = fields.Image(size_width=200, max_height = 200, required = False)
 
 
 #private String name;
@@ -48,3 +58,32 @@ class category(models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+
+class label(models.Model):
+    _name = 'salespop.label'
+    _description = 'salespop label'
+
+    name = fields.Char()
+    product = fields.One2many('salespop.product', 'label')
+
+class message(models.Model):
+    _name = 'salespop.message'
+    _description = 'salespop mensaje'
+
+    content = fields.Char()
+    product = fields.Many2one('selpop.product', ondelete = "cascade")
+
+class employee(models.Model):
+    _name = 'salespop.message'
+    _description = 'salespop mensaje'
+
+    name = fields.Char()
+
+
+class photo(models.Model):
+    _name = 'salespop.photo'
+    _description = 'salespop photo articulo'
+
+    photo = fields.Image(size_width=200, max_height = 200, required = True)
+    product = fields.Many2one('salespop.product', ondelete = "cascade")
